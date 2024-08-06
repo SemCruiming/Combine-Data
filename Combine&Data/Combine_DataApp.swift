2d29e0e280171dfe073b24a0c8123d44db8de54e
Sem C <semcruiming@gmail.com>
2024-08-06 18:58:35 +0200

    Initial Commit

---
+++ Combine&Data/Combine_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_DataApp.swift
+//  Combine&Data
+//
+//  Created by Sem Cruiming on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Combine_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
